8135bbf4d60916538e5591e63d97f9fb5477c256
Ravidu Dilshan <36620171+RavinduDilshan@users.noreply.github.com>
2024-11-30 00:52:58 +0530

    query connected and data fetched

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Layout } from '../components';
-import { gql } from '@apollo/client';
+import { useQuery, gql } from '@apollo/client';
 
 const TRACKS = gql`
 query GetTracks {
@@ -25,7 +25,10 @@ query GetTracks {
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } =useQuery(TRACKS);
+  if(loading) return 'Loading...';
+  if(error) return `ERROR! ${error.message}`;
+  return <Layout grid>{JSON.stringify(data)} </Layout>;
 };
 
 export default Tracks;
